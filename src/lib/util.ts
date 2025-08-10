4e4aecd976ed2a7b6fd81ca499aab725f2e99c38
leonardokallucci <leonardokallucci@gmail.com>
2025-08-10 08:29:28 -0300

    Update util.ts

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,4 +1,5 @@
 export function genCoupon(amount: number) {
   const chunk = () => Math.random().toString(36).substring(2, 6).toUpperCase()
+  // Ex.: KAL-150-ABCD-EFGH
   return `KAL-${amount}-${chunk()}-${chunk()}`
 }
